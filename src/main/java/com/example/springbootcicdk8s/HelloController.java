6f245a5d9818c643044b9cf78ffdcbdd47ddaf2c
Rahul Kumar <rahulkumarvirat1510@gmail.com>
2026-04-24 22:50:25 +0530

    genai=yes

--- src/main/java/com/example/springbootcicdk8s/HelloController.java
+++ src/main/java/com/example/springbootcicdk8s/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Spring RAHUL Boot CI/CD on Kubernetes!";
+        return "Hello from Spring RAHUL kumar Boot CI/CD on Kubernetes!";
     }
     @GetMapping("/health")
     public String health() {
