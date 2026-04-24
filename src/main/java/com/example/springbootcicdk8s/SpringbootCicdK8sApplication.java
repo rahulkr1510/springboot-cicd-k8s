2e21dc01a21728b69bea58f7ce2628f7f86af182
Rahul Kumar <rahulkumarvirat1510@gmail.com>
2026-04-24 12:22:21 +0530

    first spring

---
+++ src/main/java/com/example/springbootcicdk8s/SpringbootCicdK8sApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicdk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCicdK8sApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootcicdk8s/SpringbootCicdK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicdk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdK8sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
